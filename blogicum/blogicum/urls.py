91d127e3993e683308291de54e7c60d6b5142502
Forget-me-not-crossyroad <milleralex629@gmail.com>
2023-07-06 16:34:31 +0300

    Apply isort changes

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,10 +1,7 @@
+from blog.views import page_not_found_view
+from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import (
-    handler404
-)
-from blog.views import page_not_found_view
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
